0f6a88d6ffcd0644b374ce3011237f246326b7aa
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-07-31 14:25:47 +0900

    Feat: 전역 에러 처리 시스템 구현

---
+++ be/utils/customError.js
@@ -0,0 +1,56 @@
+// utils/customError.js
+class CustomError extends Error {
+  constructor(message, statusCode = 500) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class BadRequestError extends CustomError {
+  constructor(message = "잘못된 요청입니다.") {
+    super(message, 400);
+  }
+}
+
+class UnauthorizedError extends CustomError {
+  constructor(message = "인증되지 않은 사용자입니다.") {
+    super(message, 401);
+  }
+}
+
+class ForbiddenError extends CustomError {
+  constructor(message = "접근 권한이 없습니다.") {
+    super(message, 403);
+  }
+}
+
+class NotFoundError extends CustomError {
+  constructor(message = "리소스를 찾을 수 없습니다.") {
+    super(message, 404);
+  }
+}
+
+class ConflictError extends CustomError {
+  constructor(message = "데이터 충돌이 발생했습니다.") {
+    super(message, 409);
+  }
+}
+
+class ExternalServiceError extends CustomError {
+  constructor(message = "외부 서비스 처리 중 오류가 발생했습니다.", statusCode = 500) {
+    super(message, statusCode);
+    this.name = "ExternalServiceError";
+  }
+}
+
+export {
+  CustomError,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
+  ExternalServiceError,
+};
