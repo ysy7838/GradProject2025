e19890236d55cee6876a548bdbfb09f0002a64cd
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-08-02 03:19:19 +0900

    Feat: 메인 파일 추가 및 패키지 수정

---
+++ be/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"
+
+import connect from "./config/db.js";
+import categoryRoutes from "./modules/categories/category.routes.js";
+import userRoutes from "./modules/users/user.routes.js";
+import memoRoutes from "./modules/memos/memo.routes.js";
+import errorHandler from "./middlewares/errorHandler.js"
+
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5000",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    maxAge: 86400,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded());
+
+// DB 연결
+connect();
+
+// 라우트 설정
+app.use("/api/categories", categoryRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/memos", memoRoutes);
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT} (env: ${process.env.NODE_ENV})`)
+);
