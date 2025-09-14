ff7262a61ec301b485d1c95c3861344a68201d7a
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-09-15 01:42:52 +0900

    Refactor: 프로젝트 이름 변경 및 의존성 업데이트
    - package.json에서 프로젝트 이름을 "my-vite-app"에서 "smart-memo"로 변경.
    - Vite를 사용한 빌드에 앞서 TypeScript 컴파일 과정이 포함되도록 빌드 스크립트를 업데이트.
    - 상태 관리, 폼 핸들링, 스타일링을 위한 새로운 의존성을 추가하고, React와 React DOM 의존성을 최신 버전으로 교체.
    - TypeScript, ESLint, Tailwind CSS 및 기타 필요한 도구들을 포함하도록 개발 - 의존성(devDependencies)을 업데이트.
    - Tailwind CSS와 Autoprefixer를 위한 PostCSS 설정을 추가.
    - 애플리케이션과 Node.js용 TypeScript 설정 파일을 분리하여 생성.
    - Vite 설정을 JavaScript에서 TypeScript로 마이그레이션하고, 매니페스트를 통한 PWA 지원을 추가
    - 개발 중 API 요청을 위한 서버 프록시를 설정.

---
+++ fe/my-vite-app/vite.config.ts
@@ -0,0 +1,93 @@
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import {VitePWA} from "vite-plugin-pwa";
+import path from "path";
+
+export default defineConfig(() => {
+  const plugins = [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["images/icon.svg"],
+      manifest: {
+        short_name: "SmartMemo",
+        name: "SmartMemo - 똑똑한 메모",
+        description: "메모를 편리하고 간편하게",
+        icons: [
+          {
+            src: "/images/icon.svg",
+            type: "image/svg+xml",
+            sizes: "any",
+            purpose: "any maskable",
+          },
+        ],
+        id: "/",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#52C39C",
+        background_color: "#ffffff",
+        orientation: "any",
+        categories: ["productivity", "utilities"],
+        lang: "ko-KR",
+        dir: "ltr",
+        prefer_related_applications: false,
+        scope: "/",
+        display_override: ["window-controls-overlay"],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ];
+
+  return {
+    publicDir: "public",
+    plugins,
+    resolve: {
+      alias: [{find: "@", replacement: path.resolve(__dirname, "src")}],
+    },
+    build: {
+      assetsDir: "assets",
+      outDir: "dist",
+      rollupOptions: {
+        input: {app: path.resolve(__dirname, "index.html")},
+        output: {
+          assetFileNames: (assetInfo) => {
+            const info = assetInfo.name.split(".");
+            const ext = info[info.length - 1];
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+              return `assets/images/[name]-[hash][extname]`;
+            }
+            return `assets/[name]-[hash][extname]`;
+          },
+        },
+      },
+      emptyOutDir: true,
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          configure: (proxy) => {
+            proxy.on("error", (err) => {
+              console.log("proxy error", err);
+            });
+            proxy.on("proxyReq", (proxyReq, req) => {
+              console.log("Sending Request to the Target:", req.method, req.url);
+            });
+            proxy.on("proxyRes", (proxyRes, req) => {
+              console.log("Received Response from the Target:", proxyRes.statusCode, req.url);
+            });
+          },
+        },
+      },
+    },
+  };
+});
